4b4a65866468a2835854af02d2b337e508cfa64d
J <jakemckenzie19@gmail.com>
2017-01-14 20:42:13 -0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,4 @@
-var pContainerHeight = $('.para-box').height();
-
-$(window).scroll(function(){
-
-  var wScroll = $(this).scrollTop();
-
-  if (wScroll <= pContainerHeight) {
-
-    $('.para-text').css({
-      'transform' : 'translate(0px, '+ wScroll /2 +'%)'
-    });
-}
-
-
-
-  // Landing Elements
-  if(wScroll > $('.grid-pics').offset().top - ($(window).height() / 1.2)) {
-
-    $('.grid-pics figure').each(function(i){
-
-      setTimeout(function(){
-        $('.grid-pics figure').eq(i).addClass('is-showing');
-      }, (700 * (Math.exp(i * 0.14))) - 700);
-    });
-
-
-
 // Scroll to element
-
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
