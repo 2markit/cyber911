4aeb305b200f480c6eeeeb9a5494b811b6c7935c
J <jakemckenzie19@gmail.com>
2017-01-17 23:20:34 -0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -5,27 +5,9 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
       $('html, body').animate({
-        scrollTop: target.offset().top
+        scrollTop: target.offset().top -75
       }, 1000);
       return false;
     }
   }
 });
-
-var timer;
-
-$(".drop-menu").on("mouseover", function() {
-  clearTimeout(timer);
-  openSubmenu();
-}).on("mouseleave", function() {
-  timer = setTimeout(
-    closeSubmenu
-  , 1000);
-});
-
-function openSubmenu() {
-  $(".submenu").addClass("visible");
-}
-function closeSubmenu() {
-  $(".submenu").removeClass("visible");
-}
